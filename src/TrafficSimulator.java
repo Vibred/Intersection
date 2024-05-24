bd7e7d6be04c5e003ee81ac493abd9c856351a18
3122004716 <tsengvee@duck.com>
2024-05-25 00:35:21 +0800

    commit v2

--- src/TrafficSimulator.java
+++
@@ -1,7 +0,0 @@
-// TrafficSimulator.java
-public class TrafficSimulator {
-    public static void main(String[] args) throws InterruptedException {
-        Intersection intersection = new Intersection();
-        intersection.startSimulation(10, 5);
-    }
-}
