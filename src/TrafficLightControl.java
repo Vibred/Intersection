0cad5d5b39b5ef4b570b309e842fc0563531c37d
3122004716 <tsengvee@duck.com>
2024-05-25 13:30:10 +0800

    commit v3

--- src/TrafficLightControl.java
+++ src/TrafficLightControl.java
@@ -11,6 +11,7 @@ public class TrafficLightControl {
 
     // 返回当前信号灯状态
     public String getCurrentLight(String direction, String turnDirection) {
+        //红绿灯情况
         switch (currentState) {
             case S_N_STRAIGHT:
                 if (direction.equals("南向") || direction.equals("北向")) {
